210b52d4b55c9595051b3846cbb657cf3b90dbc3
Hamsa Dixit <hamsadixit202@gmail.com>
2026-04-28 01:42:24 +0530

    fix 500 error

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://task-manager-backend-r72q.onrender.com",
-  baseURL: "http://localhost:5000",
   withCredentials: true
 });
 
